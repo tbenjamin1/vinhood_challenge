114495a8c29801e8153bad50e0b4bd1972d7ab14
TUYISINGIZE Benjamin <tbenpollyl@gmail.com>
2022-10-04 17:28:28 +0200

    challenge -features:component holds card displays breweries infomation

---
+++ src/components/Breweries/index.tsx
@@ -0,0 +1,16 @@
+import Brewery from './Brewery'
+
+const Breweries = (props: { breweries: any[] }) => {
+    console.log(props.breweries)
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {props.breweries.map(
+                (brewry: { id?: any; name?: string; state?: string }) => (
+                    <Brewery key={brewry.id} brewry={brewry} />
+                )
+            )}
+        </div>
+    )
+}
+
+export default Breweries
