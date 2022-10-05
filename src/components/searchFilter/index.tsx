43fd235dc0ab53da3c3d9f404de0abc0bc6004c4
TUYISINGIZE Benjamin <tbenpollyl@gmail.com>
2022-10-05 08:23:57 +0200

    challenge-feature:api integration and filtering response with multiple filters

---
+++ src/components/searchFilter/index.tsx
@@ -0,0 +1,140 @@
+import Collapse from '@components/Collapse'
+import { fetchBreweries } from '@redux-slices/breweries.slice'
+import { useAppDispatch } from '@store/hooks'
+import { RootState } from '@store/store'
+import { FormEvent, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+const TYPES = [
+    'micro',
+    'nano',
+    'regional',
+    'brewpub',
+    'large',
+    'planning',
+    'bar',
+    'contract',
+    'proprietor',
+    'closed',
+]
+
+const SearchFilter = () => {
+    const history = useHistory()
+    const dispatch = useAppDispatch()
+    const [city, setCity] = useState('')
+    const [type, setType] = useState(TYPES[0])
+    const [name, setName] = useState('')
+    const [sort, setSort] = useState(0)
+    const [perPage, setPerPage] = useState(5)
+    const breweriesStatus = useSelector(
+        (state: RootState) => state.breweries.status
+    )
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        await dispatch(
+            fetchBreweries({
+                city,
+                type,
+                name,
+                sort: sort ? 'asc' : 'desc',
+                perPage,
+            })
+        ).unwrap()
+
+        history.push('/results')
+    }
+
+    return (
+        <div>
+            <form className="py-4" onSubmit={handleSubmit}>
+                <Collapse title="Filter By">
+                    <div className="form-control my-3">
+                        <label htmlFor="city" className="label">
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="eg: Camilia"
+                            className="input input-bordered"
+                        />
+                    </div>
+                    <div className="form-control my-3">
+                        <label htmlFor="city" className="label">
+                            City
+                        </label>
+                        <input
+                            type="text"
+                            value={city}
+                            onChange={(e) => setCity(e.target.value)}
+                            placeholder="eg: New York"
+                            className="input input-bordered"
+                        />
+                    </div>
+                    <div className="form-control my-3">
+                        <label htmlFor="type" className="label">
+                            Type
+                        </label>
+                        <select
+                            id="type"
+                            value={type}
+                            onChange={(e) => setType(e.target.value)}
+                            className="select select-bordered capitalize w-full"
+                        >
+                            {TYPES.map((type) => (
+                                <option value={type} key={type}>
+                                    {type}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="form-control my-3">
+                        <label
+                            htmlFor="sort"
+                            className="label justify-start gap-2"
+                        >
+                            <span>Desc</span>
+                            <input
+                                type="checkbox"
+                                value={sort}
+                                onChange={(e) => setSort(+e.target.value)}
+                                id="sort"
+                                className="toggle"
+                            />
+                            <span>Asc</span>
+                        </label>
+                    </div>
+                </Collapse>
+                <Collapse title="Per page">
+                    <label className="label">Per Page: {perPage}</label>
+                    <input
+                        type="range"
+                        value={perPage}
+                        onChange={(e) => setPerPage(+e.target.value)}
+                        min="0"
+                        max="100"
+                        className="range"
+                        step="5"
+                    />
+                </Collapse>
+                <div className="my-3 flex justify-end gap-2">
+                    <button className="btn" type="reset">
+                        Cancel
+                    </button>
+                    <button
+                        className={` ${
+                            breweriesStatus === 'loading' && 'loading'
+                        } btn btn-primary`}
+                        type="submit"
+                    >
+                        Apply
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default SearchFilter
---
+++ src/redux-slices/breweries.slice.ts
@@ -0,0 +1,50 @@
+import IBreweriesPayload from '@interfaces/IBreweriesPayload'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { RootState } from '@store/store'
+import axios from 'axios'
+
+type InitialStateType = {
+    breweries: []
+    status: 'idle' | 'loading' | 'succeeded' | 'failed'
+    error: string | null
+}
+
+const initialState: InitialStateType = {
+    breweries: [],
+    status: 'idle',
+    error: null,
+}
+
+export const breweriesSlice = createSlice({
+    name: 'breweries',
+    initialState,
+    reducers: {},
+    extraReducers(builder) {
+        builder
+            .addCase(fetchBreweries.pending, (state, _action) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchBreweries.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                state.breweries = action.payload
+            })
+            .addCase(fetchBreweries.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message as string
+            })
+    },
+})
+
+export default breweriesSlice.reducer
+
+export const getBreweries = (state: RootState) => state.breweries.breweries
+
+export const fetchBreweries = createAsyncThunk(
+    'breweries/fetchBreweries:load',
+    async (payload: IBreweriesPayload) => {
+        const response = await axios.get(
+            `https://api.openbrewerydb.org/breweries?by_city=${payload.city}&by_name=${payload.name}&per_page=${payload.perPage}&by_type=${payload.type}&sort=type,name:${payload.sort}`
+        )
+        return response.data
+    }
+)
