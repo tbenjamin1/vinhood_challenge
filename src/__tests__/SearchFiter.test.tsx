413e831535df22fedb105244bb1bb7eccb31dc60
TUYISINGIZE Benjamin <tbenpollyl@gmail.com>
2022-10-07 12:45:19 +0200

    test case for checking if should dispatch an action on button click

--- src/__tests__/SearchFiter.test.tsx
+++ src/__tests__/SearchFiter.test.tsx
@@ -1,66 +1,82 @@
 import React from "react";
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import {render,screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { fetchBreweries } from '@redux-slices/breweries.slice'
 const mockStore = configureStore([]);
 
 import SearchFilter from "../components/searchFilter/index";
 
-it('render filter page',() => {
- let  store = mockStore({
-      myState: 'filter test',
+it('render filter page', () => {
+  let store = mockStore({
+    myState: 'filter test',
+    breweries: {
+      breweries: [],
+      status: 'idle',
+      error: null,
+    },
+  });
+  render(<Provider store={store}><SearchFilter /></Provider>);
+  expect(screen.getByTestId('filter'))
+})
+
+describe('My Connected React-Redux Component', () => {
+  let store: any;
+  let component: any;
+  let buttonApply: any;
+
+  beforeEach(() => {
+    store = mockStore({
+      myState: 'sample text',
       breweries: {
         breweries: [],
         status: 'idle',
         error: null,
       },
     });
-   render(<Provider store={store}><SearchFilter/></Provider>);
-   expect(screen.getByTestId('filter'))
-})
 
-describe('My Connected React-Redux Component', () => {
-    let store:any;
-    let component:any;
-    let buttonApply:any;
-  
-    beforeEach(() => {
-      store = mockStore({
-        myState: 'sample text',
-        breweries: {
-            breweries: [],
-            status: 'idle',
-            error: null,
-          },
-      });
-  
-      component = render(
-        <Provider store={store}>
-          <SearchFilter />
-        </Provider>
-      );
+    component = render(
+      <Provider store={store}>
+        <SearchFilter />
+      </Provider>
+    );
 
-      buttonApply= screen.getByRole('button',{ name:/Apply/i})
-    });
-  
-    it('apply button should be defined ', () => {
-      expect(buttonApply).toBeDefined() ;
-    });
-
-    it('should render with given state from Redux store', () => {
-      expect(component).toMatchSnapshot();
-    });
-
-    
-
- 
-  
-    
+    buttonApply = screen.getByRole('button', { name: /Apply/i })
   });
- 
+
+  it('apply button should be defined ', () => {
+    expect(buttonApply).toBeDefined();
+  });
+
+  it('should render with given state from Redux store', () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should dispatch an action on button click', () => {
+
+    component.root.findByType('button').props.onClick();
+
+    component.root.findByType('input')
+      .props.onChange({ target: { value: 'satte name' } });
+
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+    expect(store.dispatch).toHaveBeenCalledWith(
+      fetchBreweries({
+        city: 'kigali',
+        type: 'micro',
+        name: 'brewery',
+        sort: 'desc',
+        perPage: 6,
+      })
+    );
+  });
 
 
 
 
-  
+});
+
+
+
+
+
